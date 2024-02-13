6c0bd2eec15ad4502cbb78d129e27a7de2fd00b9
Aakriti <aakritiad01@gmail.com>
2024-02-13 14:31:52 +0545

    change navbar

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -7,36 +7,35 @@ function Navbar() {
     setNav(!nav);
   };
   return (
-    <div className="text-white flex justify-between items-center mx-w-[1240px] h-24 px-4 mx-auto ">
-      <h1 className="text-3xl font-bold text-[#00df9a] w-full">REACT.</h1>
-      <ul className="hidden md:flex">
-        <li className="p-8 cursor-pointer">HOME</li>
-        <li className="p-8 cursor-pointer"> ABOUT</li>
-        <li className="p-8 cursor-pointer">PROJECTS</li>
-        <li className="p-8 cursor-pointer">CONTACT</li>
-      </ul>
+    <div className="text-white flex justify-between items-center mx-w-[1240px] h-24 px-4 mx-auto">
+      <h1 className="font-bold text-[#00df9a] text-3xl w-full cursor-pointer px-2 ">
+        REACT.
+      </h1>
+      <div className="hidden md:flex font-bold">
+        <div className="px-12 cursor-pointer">Home</div>
+        <div className="px-12 cursor-pointer">About</div>
+        <div className="px-12 cursor-pointer">Projects</div>
+        <div className="px-12 cursor-pointer">Contact</div>
+      </div>
       <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[45%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
+            ? "fixed top-0 left-0 border-r h-full border-r-gray-900 bg-[#000300] w-[45%]"
+            : "fixed left-[-100%] "
         }
       >
-        <h1 className="text-3xl font-bold text-[#00df9a] w-full m-8">REACT.</h1>
-
-        <ul className="p-4">
-          <li className="p-4 border-b border-gray-600 cursor-pointer">HOME</li>
-          <li className="p-4 border-b border-gray-600 cursor-pointer">ABOUT</li>
-          <li className="p-4 border-b border-gray-600 cursor-pointer">
-            PROJECTS
-          </li>
-          <li className="p-4 border-b border-gray-600 cursor-pointer">
-            CONTACT
-          </li>
-        </ul>
+        <div className="m-8">
+          <h1 className="font-bold text-[#00df9a] text-3xl ease-in-out duration-5600 w-full cursor-pointer mb-7">
+            REACT.
+          </h1>
+          <div className="p-4 cursor-pointer">Home</div>
+          <div className="p-4 cursor-pointer">About</div>
+          <div className="p-4 cursor-pointer">Projects</div>
+          <div className="p-4 cursor-pointer">Contact</div>
+        </div>
       </div>
     </div>
   );
